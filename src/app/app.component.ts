8b8a6569ecf19632fac2417550a82c3e2605d48b
kumar2014 <kumaravel.au@gmail.com>
2023-01-19 19:07:45 +0530

    4 files modified, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, VERSION } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  // #8
+  constructor(private http: HttpClient) {}
+  // #9   onProductCreate(products: This products going to an ngform object
+  onProductCreate(products: { pName: string; desc: string; price: string }) {
+    console.log(products);
+    const headers = new HttpHeaders({ myHrader: 'procademy' });
+    this.http
+      .post(
+        'https://angularbykumar-default-rtdb.firebaseio.com/products.json',
+        products,
+        { headers }
+      )
+      .subscribe((res) => {
+        console.log(res);
+      });
+  }
 }
