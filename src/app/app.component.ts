1a1c399776f94c2a78406a309c4c087fa75c67f2
kumar2014 <kumaravel.au@gmail.com>
2023-01-19 17:03:33 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,9 +9,9 @@ import { product } from './model/products';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'AngularHttpRequest';
   allProducts: product[] = [];
-
+  isFetching: boolean = false;
+  // #8
   constructor(private http: HttpClient) {}
   // #9   onProductCreate(products: This products going to an ngform object
 
@@ -19,17 +19,50 @@ export class AppComponent {
   ngOnInit() {
     this.fetchProduct();
   }
-
   // #12
   onProductFetch() {
-    // Ref Previous lession
+    this.fetchProduct();
   }
   onProductCreate(products: { pName: string; desc: string; price: string }) {
     console.log(products);
-    // Please refer Privious Chapeter this section code there
+    const headers = new HttpHeaders({ myHrader: 'procademy' });
+    this.http
+      .post<{ name: string }>(
+        'https://angularbykumar-default-rtdb.firebaseio.com/products.json',
+        products,
+        { headers }
+      )
+      .subscribe((res) => {
+        console.log(res);
+      });
   }
-
-  private fetchProduct() {}
+  // #10
+  private fetchProduct() {
+    this.isFetching = true;
+    this.http
+      .get<{ [key: string]: product }>(
+        'https://angularbykumar-default-rtdb.firebaseio.com/products.json'
+      )
+      // #13 To diplay jon object in web browser
+      // This Pipe methos allows us to transform the data before it reaches this subscribe method
+      .pipe(
+        map((res) => {
+          const products = [];
+          for (const key in res) {
+            if (res.hasOwnProperty(key)) {
+              products.push({ ...res[key], id: key });
+            }
+          }
+          return products;
+        })
+      )
+      // #12
+      .subscribe((products) => {
+        console.log(products);
+        this.allProducts = products;
+        this.isFetching = false;
+      });
+  }
   onDeleteProduct(id: string) {
     this.http
       .delete(
@@ -47,3 +80,5 @@ export class AppComponent {
       .subscribe();
   }
 }
+
+// Note: We should reload the page after deleteing button submit to see the result
