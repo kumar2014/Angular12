c216edcf8c7c30a5148c4658ab3fe548beca6e8e
kumar2014 <kumaravel.au@gmail.com>
2023-01-19 15:12:45 +0530

    2 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, VERSION } from '@angular/core';
+import { map } from 'rxjs';
+import { product } from './model/products';
 
 @Component({
   selector: 'my-app',
@@ -7,19 +9,49 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  allProducts: product[] = [];
+  // #8
   constructor(private http: HttpClient) {}
   // #9   onProductCreate(products: This products going to an ngform object
+
+  // #11 When the page loades we want to display all the products which we have in the database
+  ngOnInit() {
+    this.fetchProduct();
+  }
+  // #12
+  onProductFetch() {
+    this.fetchProduct();
+  }
   onProductCreate(products: { pName: string; desc: string; price: string }) {
     console.log(products);
-    const headers = new HttpHeaders({ myHrader: 'procademy' });
+    // Please refer Privious Chapeter this section code there
+  }
+  // #10
+  private fetchProduct() {
     this.http
-      .post(
-        'https://angularbykumar-default-rtdb.firebaseio.com/products.json',
-        products,
-        { headers }
+      .get<{ [key: string]: product }>(
+        'https://angularbykumar-default-rtdb.firebaseio.com/products.json'
       )
-      .subscribe((res) => {
-        console.log(res);
+      // #13 To diplay jon object in web browser
+      // This Pipe methos allows us to transform the data before it reaches this subscribe method
+      .pipe(
+        map((res) => {
+          const products = [];
+          for (const key in res) {
+            if (res.hasOwnProperty(key)) {
+              products.push({ ...res[key], id: key });
+            }
+          }
+          return products;
+        })
+      )
+      // #12
+      .subscribe((products) => {
+        console.log(products);
+        this.allProducts = products;
       });
   }
 }
+
+// get<{ [key: string]: product }> we are Specifying the type of the response which this get method is going to return
+// So here the response is going to the type onject and this object is having a key type string and that key its going to hava a value and this value is goig to ba a type product it has in product.ts
