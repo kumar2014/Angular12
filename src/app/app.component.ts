519b86cb118d4be0b0689da70cd177c2afeebbf6
kumar2014 <kumaravel.au@gmail.com>
2023-01-19 19:28:29 +0530

    4 files modified, 1 added, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,82 @@
-import { Component, VERSION } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { map, pipe } from 'rxjs';
+import { product } from './model/products';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent implements OnInit {
+  allProducts: product[] = [];
+  isFetching: boolean = false;
+  // #8
+  constructor(private http: HttpClient) {}
+  // #9   onProductCreate(products: This products going to an ngform object
+
+  // #11 When the page loades we want to display all the products which we have in the database
+  ngOnInit() {
+    this.fetchProduct();
+  }
+  // #12
+  onProductFetch() {
+    this.fetchProduct();
+  }
+  onProductCreate(products: { pName: string; desc: string; price: string }) {
+    console.log(products);
+    const headers = new HttpHeaders({ myHrader: 'procademy' });
+    this.http
+      .post<{ name: string }>(
+        'https://angularbykumar-default-rtdb.firebaseio.com/products.json',
+        products,
+        { headers }
+      )
+      .subscribe((res) => {
+        console.log(res);
+      });
+  }
+  // #10
+  private fetchProduct() {
+    this.isFetching = true;
+    this.http
+      .get<{ [key: string]: product }>(
+        'https://angularbykumar-default-rtdb.firebaseio.com/products.json'
+      )
+      // #13 To diplay jon object in web browser
+      // This Pipe methos allows us to transform the data before it reaches this subscribe method
+      .pipe(
+        map((res) => {
+          const products = [];
+          for (const key in res) {
+            if (res.hasOwnProperty(key)) {
+              products.push({ ...res[key], id: key });
+            }
+          }
+          return products;
+        })
+      )
+      // #12
+      .subscribe((products) => {
+        console.log(products);
+        this.allProducts = products;
+        this.isFetching = false;
+      });
+  }
+  onDeleteProduct(id: string) {
+    this.http
+      .delete(
+        'https://angularbykumar-default-rtdb.firebaseio.com/products/' +
+          id +
+          '.json'
+      )
+      .subscribe();
+  }
+  onDeleteAllProduct() {
+    this.http
+      .delete(
+        'https://angularbykumar-default-rtdb.firebaseio.com/products.json/'
+      )
+      .subscribe();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule, // Step:#3
+    FormsModule,
+    // #7 We want to send a post request to the server & with the post request we want to send to these data and using these data we want to create a new product in the
+    // In Angular no matter which type of http request we are making the first thing which yo need todo specify the HttpClientModule
+    HttpClientModule,
+  ],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
