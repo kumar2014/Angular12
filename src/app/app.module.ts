85bc2224a1b321393386930394524a123b9efb7c
kumar2014 <kumaravel.au@gmail.com>
2023-01-20 23:16:47 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,7 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  imports: [
-    BrowserModule, // Step:#3
-    FormsModule,
-    // #7 We want to send a post request to the server & with the post request we want to send to these data and using these data we want to create a new product in the
-    // In Angular no matter which type of http request we are making the first thing which yo need todo specify the HttpClientModule
-    HttpClientModule,
-  ],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
