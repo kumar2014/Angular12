3d289772fcdaa2a755934d1fdb9c700aefedd0b8
kumar2014 <kumaravel.au@gmail.com>
2023-01-21 01:30:47 +0530

    2 files modified

--- src/app/Service/product.service.ts
+++ src/app/Service/product.service.ts
@@ -11,14 +11,20 @@ export class ProductService {
   error = new Subject<string>(); //It going to emit error message ad the error message its going to be a string value
   allProducts: product[] = [];
   constructor(private http: HttpClient) {}
+
+  // Method  1:Setting Http Headers
+  // Here we are creating this header we simply passed an object to this header constructor this HttpHeaders constructors
   createProduct(products: { pName: string; desc: string; price: string }) {
     console.log(products);
+
+    // #1.1 We are creating a header using new HttpHeader class and the constructor of the HttpHeaders we are passing anonymous object
+    // and here we setting the header by using a key value pair and, We are passing the this header with this post request by specifying  a third parameter for this post method
     const headers = new HttpHeaders({ myHrader: 'procademy' });
     this.http
       .post<{ name: string }>(
         'https://angularbykumar-default-rtdb.firebaseio.com/products.json',
         products,
-        { headers }
+        { headers } //#2      We are speicifying the headers which we want to send with this request
       )
       .subscribe(
         (res) => {
@@ -31,10 +37,23 @@ export class ProductService {
         }
       );
   }
+
+  // Method 2: Setting Http Headers
+  // Here we are creating a instance of this HttpHeaders and then on that instance we are calling set method,
+  // Note: this HttpHeaders is immutable
+  // This set method returns a new instance after modifying the given header and if the headeralready exits the when we use set method the header value is replaced with the given value in the returned object
+
   fetchProduct() {
+    // #2.1 Create a varible header use HttpHeader
+    const header = new HttpHeaders()
+      // #2.2 In this http headers we can call a method called set and inside this method we can specify the header which we want to send as a key value pair
+      .set('content-type', 'application/json')
+      .set('Access-Control-Allow-Origin', '*');
+    // #2.3 the header will be always passed as the last argument for the method so this get method taking only one argument which this url, so here we can specify an anonymous object  'https://angularbykumar-default-rtdb.firebaseio.com/products.json',{ headers: header }
     return this.http
       .get<{ [key: string]: product }>(
-        'https://angularbykumar-default-rtdb.firebaseio.com/products.json'
+        'https://angularbykumar-default-rtdb.firebaseio.com/products.json',
+        { headers: header }
       )
 
       .pipe(
@@ -49,12 +68,22 @@ export class ProductService {
         })
       );
   }
+  // Method:3 Setting Http Headers
+  // Note: When we use append method appends a new value to the existing set of values for headers and it returns a new instance
+  // the append method does not check If the value exists are not
+
   deleteProduct(id: string) {
+    // #3.1
+    let header = new HttpHeaders();
+    // #3.2
+    header = header.append('my header1', 'Value1');
+    header = header.append('my-header', 'Value2');
     this.http
       .delete(
         'https://angularbykumar-default-rtdb.firebaseio.com/products/' +
           id +
-          '.json'
+          '.json',
+        { headers: header } //#3.3
       )
       .subscribe();
   }
