010f898bced6a5a27a197c87f7b2f66331daac78
kumar2014 <kumaravel.au@gmail.com>
2023-01-21 11:09:05 +0530

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component, VERSION } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
+  // #4 pass the argument and specify the NgForm type
+  onSubmit(form: NgForm) {
+    console.log(form); // It will pass the html of that form. but we want typescript object for thar we have use ngForm on html #myForm="ngForm"
+  }
 }
