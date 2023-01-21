7c47bcdd0df91c006ac64166bd8f4e047bf34c01
kumar2014 <kumaravel.au@gmail.com>
2023-01-21 18:48:28 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,58 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  // #5 Create a new property form its going to a type NgForm on this form propert lets use ViewChild decoratorand pass local reference variable myForm
+  title = 'AngularForms';
+  defaultCountry = 'india';
+
+  firstname: string;
+  lastname: string;
+  email: string;
+  gen: string;
+  country: string;
+
+  defaultGender = 'Female';
+
+  gender = [
+    { id: '1', value: 'Male' },
+    { id: '2', value: 'Female' },
+    { id: '3', value: 'Other' },
+  ];
+
   @ViewChild('myForm') form: NgForm;
 
   onSubmit() {
-    // #6 usse this form property lets use this.form
-    console.log(this.form); // It will pass the html of that form. but we want typescript object for thar we have use ngForm on html #myForm="ngForm"
+    console.log(this.form);
+
+    this.firstname = this.form.value.personDetails.firstname;
+    this.lastname = this.form.value.personDetails.lastname;
+    this.email = this.form.value.personDetails.email;
+    this.gen = this.form.value.gender;
+    this.country = this.form.value.country;
+
+    this.form.reset();
+  }
+
+  setDefaultValues() {
+    // this.form.value.personDetails.firstname = 'John';
+    // this.form.value.personDetails.lastname = 'smith';
+    // this.form.value.personDetails.email = 'abc@example.com';
+    // this.form.setValue({
+    //   country: '',
+    //   gender: '',
+    //   hobbies: '',
+    //   personDetails: {
+    //     firstname: 'John',
+    //     lastname: 'Smith',
+    //     email: 'abc@example.com'
+    //   }
+    // })
+
+    this.form.form.patchValue({
+      personDetails: {
+        firstname: 'John',
+        lastname: 'Smith',
+        email: 'abc@example.com',
+      },
+    });
   }
 }
