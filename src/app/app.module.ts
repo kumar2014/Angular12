c07d768e1e8129b88cd923fe8405578bc7bf21d8
kumar2014 <kumaravel.au@gmail.com>
2023-01-22 14:12:23 +0530

    3 files modified, 16 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,35 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { CoursesComponent } from './courses/courses.component';
+import { CourseComponent } from './courses/course/course.component';
+import { RouterModule, Routes } from '@angular/router';
+import { CoursesService } from './Services/courses.service';
+
+const appRoutes: Routes = [
+  // {path: '', component: HomeComponent},
+  { path: '', redirectTo: 'Home', pathMatch: 'full' },
+  { path: 'Home', component: HomeComponent },
+  { path: 'About', component: AboutComponent },
+  { path: 'Contact', component: ContactComponent },
+  { path: 'Courses', component: CoursesComponent },
+  { path: 'Course/:id', component: CourseComponent },
+];
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    CoursesComponent,
+    CourseComponent,
+  ],
+  imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
+  providers: [CoursesService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
