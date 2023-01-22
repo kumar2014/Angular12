eb0f6dedabf0de0472e502d1f7ab42eae3df74fd
kumar2014 <kumaravel.au@gmail.com>
2023-01-22 18:52:48 +0530

    3 files modified, 12 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { CoursesComponent } from './courses/courses.component';
+import { HeaderComponent } from './header/header.component';
+import { SearchComponent } from './search/search.component';
+import { FilterComponent } from './filter/filter.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [
+    AppComponent,
+    CoursesComponent,
+    HeaderComponent,
+    SearchComponent,
+    FilterComponent,
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
