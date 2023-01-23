df6ab377e63148ad5627388f08350a4f9c2e7c23
kumar2014 <kumaravel.au@gmail.com>
2023-01-23 08:44:15 +0530

    3 files modified, 3 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, ElementRef, VERSION, ViewChild } from '@angular/core';
+import { DemoComponent } from './demo/demo.component';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  // Example1 :#2
+  @ViewChild('dobInput') dateOfBirth: ElementRef; // We want to assign dateOfBirth property with a refernce to local reference variable
+  @ViewChild('ageInput') age: ElementRef;
+
+  // Example2: How to access child component to parent component
+  @ViewChild(DemoComponent, { static: true }) demoComp: DemoComponent;
+
+  // Example1: #3
+  calculateAge() {
+    let birthYear = new Date(
+      this.dateOfBirth.nativeElement.value
+    ).getFullYear();
+    let currentYear = new Date().getFullYear();
+    let age = currentYear - birthYear;
+    this.age.nativeElement.value = age;
+    // console.log(this.dateOfBirth);
+    // console.log(this.age);
+  }
+  
 }
