941066779d50e9b186c71c543bccfbefb6629b72
kumar2014 <kumaravel.au@gmail.com>
2023-01-23 09:58:11 +0530

    1 file modified

--- src/app/angular/angular.component.ts
+++ src/app/angular/angular.component.ts
@@ -1,28 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { EnrollService } from '../Services/enroll.service';
 
-
 @Component({
   selector: 'app-angular',
   templateUrl: './angular.component.html',
   styleUrls: ['./angular.component.css'],
   //providers: [EnrollService]
 })
-export class AngularComponent implements OnInit {
+export class AngularComponent {
+  title = 'Angular';
 
-  
-    title="Angular";
-  
-   
-  
-    }
   // #5
-    OnEnroll(){
-      // #6 We have to create a instance of the entroll service class
-      const entrollService = new EnrollService();
-      this.entrollService.OnEnrollClicked(this.title);
-    }
-  ngOnInit() {
+  OnEnroll() {
+    // #6 We have to create a instance of the entroll service class
+    const entrollService = new EnrollService();
+    entrollService.OnEnrollClicked(this.title);
   }
-
 }
