94585431705818bb8e1da40f7b9736c68652765f
kumar2014 <kumaravel.au@gmail.com>
2023-01-23 09:02:49 +0530

    3 files modified, 7 added

---
+++ src/app/angular/angular.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { EnrollService } from '../Services/enroll.service';
+
+
+@Component({
+  selector: 'app-angular',
+  templateUrl: './angular.component.html',
+  styleUrls: ['./angular.component.css'],
+  //providers: [EnrollService]
+})
+export class AngularComponent implements OnInit {
+
+  
+    title="Angular";
+  
+    constructor(private enrollService: EnrollService){
+  
+    }
+  
+    OnEnroll(){
+      this.enrollService.OnEnrollClicked(this.title);
+    }
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/javascript/javascript.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { EnrollService } from '../Services/enroll.service';
+
+
+@Component({
+  selector: 'app-javascript',
+  templateUrl: './javascript.component.html',
+  styleUrls: ['./javascript.component.css'],
+  providers: [EnrollService]
+})
+export class JavascriptComponent implements OnInit {
+
+  title = "JavaScript";
+  constructor(private enrollService: EnrollService){
+
+}
+  ngOnInit() {
+  }
+  OnEnroll(){
+    this.enrollService.OnEnrollClicked(this.title);
+  }
+
+  
+
+}
