f48a4e36664b5fe8dafe4785d48f0bc74582f60f
kumar2014 <kumaravel.au@gmail.com>
2023-01-24 11:56:52 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,6 @@ import { EnrollService } from './Services/enroll.service';
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [EnrollService], //#2
 })
 export class AppComponent {
   // #3
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { JavascriptComponent } from './javascript/javascript.component';
 import { AngularComponent } from './angular/angular.component';
+import { EnrollService } from './Services/enroll.service';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
   declarations: [AppComponent, JavascriptComponent, AngularComponent],
   bootstrap: [AppComponent],
+  providers: [EnrollService], //#2
 })
 export class AppModule {}
