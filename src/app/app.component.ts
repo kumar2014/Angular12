ba5982688dcdb2e14326b298beed0d0a1a2e52cc
kumar2014 <kumaravel.au@gmail.com>
2023-01-24 16:18:01 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, VERSION } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'my-app',
@@ -6,5 +7,16 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+  name = 'Angular Observable' + VERSION.major;
+  myobservable = new Observable((observer)=>{
+    console.log('observable starts');
+    observer.next();
+  });
+
 }
+
+// Note:
+// The observable constructor we need to pass a callback a function and this callback function will receive an argument which will be the observer and this argument will be injected by rxjs lib
+
+// This observer is nothing but the subscriber which is waiting for the data 
+//  To emit the data on this observer we can call next method
