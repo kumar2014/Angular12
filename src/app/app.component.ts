6778a1eda564db4849603d4fe44db891a04a0d6c
kumar2014 <kumaravel.au@gmail.com>
2023-01-25 00:13:44 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, VERSION } from '@angular/core';
-import { Observable } from 'rxjs';
+import { observable, Observable } from 'rxjs';
 
 @Component({
   selector: 'my-app',
@@ -8,13 +8,13 @@ import { Observable } from 'rxjs';
 })
 export class AppComponent implements OnInit {
   name = 'Angular Observable' + VERSION.major;
-  myobservable = new Observable((observer) => {
-    console.log('observable starts');
+
+  // #1 Create method for creating observable
+  myObservable = Observable.create((observer) => {
+    observer.next('A');
     setTimeout(() => {
       observer.next('0');
     }, 3000);
-    observer.next('1');
-    // #1
     observer.error(new Error('Something Went Wrong! Please try later'));
     observer.next('3');
     observer.next('4');
@@ -23,13 +23,12 @@ export class AppComponent implements OnInit {
     }, 4000);
   });
   ngOnInit() {
-    this.myobservable.subscribe(
+    this.myObservable.subscribe(
       (val) => {
         console.log(val);
-        // #2 Second callback function for error message
       },
       (error) => {
-        // alert(error.message);
+        alert(error.message);
       },
       () => {
         alert('Observable has complete emitting all values.');
@@ -39,14 +38,3 @@ export class AppComponent implements OnInit {
 }
 
 //Note:
-// Error: When this error will be emitted after that we have 3 and 4 these two values will not get emitted by the observable
-// Whenever an observable emits an error after that if we have any values to be emittedthose values will not get emitted.
-
-// Once the complete signal is emitted from the observable after that if we have any data to be emitted that will not get emited.
-// For example
-
-// If We change the time interval 1seconds this complete signal will be emitted after 1seconds so any value which we are emitting agter 1seconds for examples this value 0 and 1 other value not get emitted
-
-// once the complete signal has been emitted from the observable after that no value will be emitted by that the observable
-
-// once the observable return the error after that it will also return a complete singnal
