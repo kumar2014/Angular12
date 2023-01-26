e27b72fac8804e49fe2a50dd6f205e9eaddf6d61
kumar2014 <kumaravel.au@gmail.com>
2023-01-26 16:06:02 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, OnInit, VERSION } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
   name = 'Angular ' + VERSION.major;
+  counterObservable = interval(1000);
+  counterSub;
+
+  ngOnInit() {}
+
+  subscribe() {
+    this.counterSub = this.counterObservable.subscribe((val) => {
+      console.log(val);
+    });
+  }
+  unSubscribe() {
+    this.counterSub.unsubscribe();
+  }
 }
