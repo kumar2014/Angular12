6c8b97505bd7d4d2a5a2736eedad0e2d6ce6fad9
kumar2014 <kumaravel.au@gmail.com>
2023-01-26 15:28:55 +0530

    1 file modified

--- src/app/Service/data.service.ts
+++ src/app/Service/data.service.ts
@@ -1,14 +1,20 @@
 import { EventEmitter, Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 // #6
 @Injectable()
 export class DataService {
-  dataEmitter = new EventEmitter<string>();
+  // creatring new subject constructor it will return an observable
+
+  dataEmitter = new Subject<string>();
+  // Specify the type of data which the observable returened by this subject will emit
 
   raiseDataEventEmitterEvent(data: string) {
-    this.dataEmitter.emit(data);
+    this.dataEmitter.next(data);
   }
 }
 
-// Note: 
+// Note:
+//
+// dataEmitter is a Subject so we need to call next method
 // We have to inject the service in the app.component class
