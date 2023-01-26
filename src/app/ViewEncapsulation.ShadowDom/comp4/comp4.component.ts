1aa5b6834d65d3f2ad6ff724ae03789aedf418e3
kumar2014 <kumaravel.au@gmail.com>
2023-01-26 20:54:40 +0530

    3 files modified, 6 added

---
+++ src/app/ViewEncapsulation.ShadowDom/comp4/comp4.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-comp4',
+  templateUrl: './comp4.component.html',
+  styleUrls: ['./comp4.component.css'],
+  encapsulation: ViewEncapsulation.ShadowDom,
+})
+export class Comp4Component implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
