6196c0a63031df094e61ddafefd156850240c453
kumar2014 <kumaravel.au@gmail.com>
2023-01-26 22:16:25 +0530

    2 files modified

--- src/app/view-encapsulation-none/view-encapsulation-none.component.ts
+++ src/app/view-encapsulation-none/view-encapsulation-none.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-view-encapsulation-none',
   templateUrl: './view-encapsulation-none.component.html',
-  styleUrls: ['./view-encapsulation-none.component.css']
+  styleUrls: ['./view-encapsulation-none.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class ViewEncapsulationNoneComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- src/app/view-encapsulation-shadow-dom/view-encapsulation-shadow-dom.component.ts
+++ src/app/view-encapsulation-shadow-dom/view-encapsulation-shadow-dom.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-view-encapsulation-shadow-dom',
   templateUrl: './view-encapsulation-shadow-dom.component.html',
-  styleUrls: ['./view-encapsulation-shadow-dom.component.css']
+  styleUrls: ['./view-encapsulation-shadow-dom.component.css'],
+  encapsulation: ViewEncapsulation.ShadowDom,
 })
 export class ViewEncapsulationShadowDomComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
