3f2fd57314aa3df9224dfb90f6afeae7498f038b
kumar2014 <kumaravel.au@gmail.com>
2023-01-26 21:09:32 +0530

    2 files modified, 6 added, 17 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
+import { Comp1Component } from '../ViewEncapsulationNone/comp1.component';
+import { Comp2Component } from '../ViewEncapsulationNone/comp2.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [
-    AppComponent,
-    Comp1Component,
-    Comp2Component,
-    Comp3Component,
-    Comp4Component,
-    Comp5Component,
-    Comp6Component,
-  ],
+  declarations: [AppComponent, Comp1Component, Comp2Component],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
