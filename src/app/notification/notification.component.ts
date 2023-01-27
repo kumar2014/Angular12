bc71e385ef611a1c4b4832099ba16456652380a6
kumar2014 <kumaravel.au@gmail.com>
2023-01-27 14:54:04 +0530

    2 files modified

--- src/app/notification/notification.component.ts
+++ src/app/notification/notification.component.ts
@@ -6,19 +6,20 @@ import { Component, OnInit } from '@angular/core';
   This website uses cookies to provide better user experience.
   <div class="close"><button class="btn" (click)="closeNotification()">X</button></div>
 </div>`,
-styles: ["div{margin: 10px 0px; padding: 10px 20px; text-align:center;}", 
-"p{font-size: 14px;}", ".close{float: right; margin-top: -15px;}",
-".fadeOut {visibility: hidden;opacity: 0; transition: visibility 0s 2s, opacity 2s linear;}"]
+  styles: [
+    "div{margin: 10px 0px; padding: 10px 20px; text-align:center;backgrond:'green'; }",
+    'p{font-size: 14px;}',
+    '.close{float: right; margin-top: -15px;}',
+    '.fadeOut {visibility: hidden;opacity: 0; transition: visibility 0s 2s, opacity 2s linear;}',
+  ],
 })
 export class NotificationComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
+  displayNotification: boolean = false;
 
-  ngOnInit() {
-  }
-  displayNotification:boolean = false;
-
-  closeNotification(){
+  closeNotification() {
     this.displayNotification = true;
   }
 }
