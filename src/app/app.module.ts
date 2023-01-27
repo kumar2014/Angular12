6d5567bcdab9b9927ec381c51cb1e064702ae174
kumar2014 <kumaravel.au@gmail.com>
2023-01-27 14:37:03 +0530

    2 files modified, 3 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,10 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
 import { ProductsComponent } from './products/products.component';
-import { SearchComponent } from './search/search.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, ProductsComponent, SearchComponent],
+  declarations: [AppComponent, ProductsComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
