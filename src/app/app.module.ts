3f75b2ee4776b505173b3405067782a7e5131a51
kumar2014 <kumaravel.au@gmail.com>
2023-01-28 16:25:36 +0530

    4 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+
+import { IfDirective } from './CustomDirective/if.directive';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, IfDirective],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
