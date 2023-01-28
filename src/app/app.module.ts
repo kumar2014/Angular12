b2313fc1127ea4da243844d9585cfff61e55675c
kumar2014 <kumaravel.au@gmail.com>
2023-01-28 15:35:02 +0530

    3 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { StyleDirective } from './CustomDirective/style.directive';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, StyleDirective],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
