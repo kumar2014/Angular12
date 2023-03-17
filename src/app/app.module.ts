b15a194a6bc449dcd8b1ce1d08d36acdaa6fde6d
kumar2014 <kumaravel.au@gmail.com>
2023-03-17 19:25:37 +0530

    2 files modified, 6 added, 4 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { ListComponent } from './list/list.component';
 
 @NgModule({
   imports: [
