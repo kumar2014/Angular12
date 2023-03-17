84955c755ebbc98f2bf980d5f1c3e4a6661ede29
kumar2014 <kumaravel.au@gmail.com>
2023-03-17 19:38:08 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,8 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { product } from './model/products';
-import { ProductService } from './Service/product.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
- 
-}
+export class AppComponent {}
