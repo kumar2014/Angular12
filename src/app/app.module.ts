36c758aec16660a8edb89dcb44519d717b737290
kumar2014 <kumaravel.au@gmail.com>
2023-03-17 19:07:46 +0530

    1 file modified, 5 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-
+import { LoginComponent } from './login/login.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   imports: [
     BrowserModule, // Step:#3
     FormsModule,
-   
   ],
-  declarations: [AppComponent],
+  declarations: [AppComponent, LoginComponent, ListComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
