61f1e314c87b2a712582a1bbcb596575a861f237
kumar2014 <kumaravel.au@gmail.com>
2023-03-17 20:15:40 +0530

    1 file modified

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin.routing.module';
+import { ListComponent } from './list/list.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   imports: [
     CommonModule,
     AdminRoutingModule
   ],
-  declarations: []
+  declarations: [LoginComponent,ListComponent]
 })
 export class AdminModule { }
