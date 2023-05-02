ede2c24ea1b8d5d9de15858763bb8228420e0783
kumar2014 <kumaravel.au@gmail.com>
2023-05-02 15:44:44 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, OnInit, VERSION } from '@angular/core';
+import { UserService } from './service/user.service';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
   name = 'Angular ' + VERSION.major;
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.onGetUsers();
+    this.onGetUser();
+  }
+  onGetUsers():void {
+    this.userService.getUsers().subscribe(
+      (response) => console.log(response),
+      (error:any) => console.log(error),
+      () => console.log('Done getting users')
+    );
+  }
+
+  onGetUser():void {
+    this.userService.getUsers().subscribe(
+      (response) => console.log(response),
+      (error:any) => console.log(error),
+      () => console.log('Done getting users')
+    );
+  }
 }
